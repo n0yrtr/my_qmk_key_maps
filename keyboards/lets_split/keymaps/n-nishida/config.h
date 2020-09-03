a16dccc968009188aaf6712c714d079979af4a28
Naoya Nishida <n-nishida@zyyx.jp>
2020-09-03 12:06:52 +0900

    Tapping Termの変更

--- keyboards/lets_split/keymaps/n-nishida/config.h
+++ keyboards/lets_split/keymaps/n-nishida/config.h
@@ -36,7 +36,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // #define EE_HANDS
 
 // hold & tapping delay setting
-#define TAPPING_TERM 200
+#define TAPPING_TERM 150
 
 
 // permissive holｄがあってTappingTermを伸ばしていれば　RETRO_TAPPINGはいらないかな。
