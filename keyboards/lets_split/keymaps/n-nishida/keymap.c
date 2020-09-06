3ed94a22016d812a16ae4340e1e0b373f118f051
Naoya Nishida <n-nishida@zyyx.jp>
2020-09-06 14:28:25 +0900

    一旦いい感じになった。

--- keyboards/lets_split/keymaps/n-nishida/keymap.c
+++ keyboards/lets_split/keymaps/n-nishida/keymap.c
@@ -34,10 +34,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_QWERTY] = LAYOUT_ortho_4x12(
-   _______,  KC_Q,    KC_W,    KC_E,           KC_R,          KC_T,           KC_Y,                 KC_U,                KC_I,             KC_O,    KC_P,    _______, \
-   _______,  KC_A,    KC_S,    KC_D,           KC_F,          KC_G,           KC_H,                 KC_J,                KC_K,             KC_L,    KC_SCLN, _______, \
-   _______,  KC_Z,    KC_X,    KC_C,           KC_V,          KC_B,           KC_N,                 KC_M,                KC_COMM,          KC_DOT,  KC_SLSH, _______, \
-   _______,  _______, _______, CTL_T(KC_LGUI), KC_LGUI,       SFT_T(KC_SPC),  LOWER,   RAISE,   RALT_T(KC_RGUI),  _______, _______, _______  \
+   _______,  KC_Q,           KC_W,    KC_E,           KC_R,          KC_T,           KC_Y,    KC_U,    KC_I,             KC_O,    KC_P,           _______, \
+   _______,  SFT_T(KC_A),    KC_S,    KC_D,           KC_F,          KC_G,           KC_H,    KC_J,    KC_K,             KC_L,    SFT_T(KC_SCLN), _______, \
+   _______,  KC_Z,           KC_X,    KC_C,           KC_V,          KC_B,           KC_N,    KC_M,    KC_COMM,          KC_DOT,  KC_SLSH,        _______, \
+   _______,  _______,        _______, CTL_T(KC_LGUI), GUI_T(KC_TAB), SFT_T(KC_SPC),  LOWER,   RAISE,   RALT_T(KC_RGUI),  _______, _______,        _______  \
 ),
 
 
@@ -54,10 +54,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_LOWER] = LAYOUT_ortho_4x12( \
   KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN,KC_RPRN, KC_DEL,  _______,\
-  KC_1,    KC_2,    KC_3,    KC_4,    KC_5, 
-  KC_6,    KC_7,    KC_8,    KC_9,   KC_0,    KC_MINS, KC_EQL, \
+  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,   KC_0,    KC_MINS, KC_EQL, \
   _______, _______, _______, _______, _______, _______, KC_GRV,  KC_QUOT, KC_LBRC,KC_RBRC, KC_BSLS, _______,\
-  _______, _______, _______, KC_ESC, KC_TAB,  _______, _______, _______, KC_MNXT,KC_VOLD, KC_VOLU, KC_MPLY \
+  _______, _______, _______, KC_ESC, KC_ESC,  _______, _______, _______, KC_MNXT,KC_VOLD, KC_VOLU, KC_MPLY \
 ),
 
 /* Raise
@@ -73,9 +72,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_RAISE] = LAYOUT_ortho_4x12( \
   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,        KC_F12,       \
-  _______, _______, _______, _______, _______, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_RALT,       KC_RGUI, \
+  _______, _______, _______, KC_LALT, KC_LGUI, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______,       _______, \
   _______, _______, _______, _______, _______, _______, _______, KC_NUHS, KC_BTN1, KC_BTN2, KC_MS_WH_UP,   _______,\
-  _______, _______, _______, RSFT(KC_LCTL), RSFT(KC_LGUI), _______, _______, _______, KC_MNXT, KC_VOLD, KC_VOLU,       KC_MPLY        \
+  _______, _______, _______, _______, _______, _______, _______, _______, KC_MNXT, KC_VOLD, KC_VOLU,       KC_MPLY        \
 ),
 
 /* Adjust (Lower + Raise)
