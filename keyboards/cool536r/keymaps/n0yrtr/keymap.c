8c65676bafb3f41f7744848dcc95b56aae9e5990
Naoya Nishida <n-nishida@zyyx.jp>
2023-01-30 13:47:34 +0900

    cool536 rotaly encoder key changes

--- keyboards/cool536r/keymaps/n0yrtr/keymap.c
+++ keyboards/cool536r/keymaps/n0yrtr/keymap.c
@@ -25,8 +25,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_Q,           KC_W,    KC_E,            KC_R,          KC_T,           KC_Y,    KC_U,    KC_I,             KC_O,    KC_P,
     KC_A,           KC_S,    KC_D,            KC_F,          KC_G,           KC_H,    KC_J,    KC_K,             KC_L,    SFT_T(KC_SCLN),
     KC_Z,           KC_X,    KC_C,            KC_V,          KC_B,           KC_N,    KC_M,    KC_COMM,          KC_DOT,  CTL_T(KC_SLSH),
-    KC_1,           KC_2,    LALT_T(KC_LGUI), GUI_T(KC_TAB), SFT_T(KC_SPC),  LOWER,   RAISE,   RALT_T(KC_RGUI),  KC_3,    KC_4,
-    KC_LEFT,        KC_RIGHT,KC_UP,           KC_DOWN,       KC_VOLD,         KC_VOLU, KC_BSPC, KC_SPC
+    KC_1,           KC_2,    LALT_T(KC_LGUI), GUI_T(KC_TAB), SFT_T(KC_SPC),  LOWER,   RAISE,   RALT_T(KC_RGUI),  KC_MPLY, KC_MNXT,
+    KC_LEFT,        KC_RIGHT,KC_UP,           KC_DOWN,       KC_VOLD,        KC_VOLU, KC_BSPC, KC_SPC
   ),
   
   [_LOWER] = LAYOUT(
@@ -34,7 +34,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,     KC_8,   KC_9,    KC_0,
     _______, _______, _______, KC_EQL,  KC_MINS,  KC_GRV,  KC_QUOT, KC_LBRC,KC_RBRC, KC_BSLS,
     _______, _______, KC_ESC, KC_ESC,   _______, _______, _______,  KC_MNXT,KC_VOLD, KC_VOLU,
-    KC_LEFT, KC_RIGHT,KC_UP,  KC_DOWN,  KC_A,    KC_B,    KC_C,     KC_D
+    KC_LEFT, KC_RIGHT,KC_UP,  KC_DOWN,  KC_BRID,   KC_BRIU,    KC_C,     KC_D
   ) ,
  
   
@@ -43,7 +43,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_F11,  KC_F12,  KC_LALT, KC_LGUI, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______,
     _______, _______, _______, _______, _______, _______, KC_NUHS, KC_BTN1, KC_BTN2, KC_MS_WH_UP,
     _______, _______, _______, _______, _______, _______, _______, KC_MNXT, KC_VOLD, KC_VOLU,
-    KC_LEFT,KC_RIGHT,KC_UP,KC_DOWN,KC_A,KC_B,KC_C,KC_D
+    KC_LEFT,KC_RIGHT,KC_UP,KC_DOWN,KC_A,KC_B,             KC_BSPC, KC_SPC
   )        
 };
 
